1a45e973e4b2eb372c45822214b7b3bbc6c1414b
Noland Seigler <nolandseigler@gmail.com>
2020-01-26 12:21:58 -0600

    tutorial complete. the domain he was using is up for sale so I had to slightly modify the file to make it work as a proof of concept

--- intro/scrape2.js
+++ intro/scrape2.js
@@ -2,24 +2,42 @@
 const request = require('request');
 //this is basically jquery for node
 const cheerio = require('cheerio');
-
+//filesystem module
+const fs = require('fs');
+const writeStream = fs.createWriteStream('post.csv'); //param is what you want to write to
+//Write Headers
+// //header was example
+// writeStream.write(`Title,Link,Date \n`);
+// //
+writeStream.write(`Links \n`);
 request('http://codedemos.com/sampleblog', (error, response, html) => {
     if (!error && response.statusCode == 200) {
         //now i can use $ like its jquery
         const $ = cheerio.load(html);
+        // //doesnt work. website down???
+        // $('.post-preview').each((i, el) => {
+        //     const title = $(el)
+        //         .find('.post-title')
+        //         .text()
+        //         //regex to remove unnecessary white space
+        //         .replace(/\s\s+/g, '');
+        //     const link = $(el)
+        //         .find('a')
+        //         .attr('href');
+        //     const date = $(el)
+        //         .find('.post-date')
+        //         .text()
+        //         .replace(/,/, '');
+        //     console.log(`${title}, ${link}, ${date}`)
+        //     //Write Row To CSV
+        //     writeStream.write(`${title}, ${link}, ${date} \n`);
+        // });
+        // //
+        $('.kwds ul li a').each((i, el) => {
+            const href = $(el).attr('href');
+            writeStream.write(`${href}-${i} \n`)
+        });
 
-        const siteHeading = $('.site-heading');
-        const siteHeadingHtml = siteHeading.html();
-        // console.log(siteHeading);
-        // console.log(siteHeadingHtml);
-        // const output = siteHeading.find('h1').text();
-        // const outputNext = siteHeading.children('h1').next().text();
-        // console.log(output);
-        // console.log(outputNext);
-        $('.nav-item a').each((i, element) => {
-            const item = $(element).text();
-            const link = $(element).attr('href');
-            console.log(item);
-        })
+        console.log('Scraping Done...')
     }
 });
